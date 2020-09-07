b47455664a16326a89c0f80c817dc52c93e288b4
Larenthios <hugo.caille@epitech.eu>
2020-09-07 15:20:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,39 +6,33 @@ import asyncio
 import json
 import os
 
-client = discord.Client()
-triggerchar = '?'
 config = configparser.ConfigParser()
 config.read('config.ini')
 token = config['keys']['discord']
+
+bot = commands.Bot(command_prefix='?', description='Small bot')
 
 if (not os.path.exists("fights")):
 	os.mkdir("fights")
 
 #Asynchronous tasks
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-	if message.content.startswith(triggerchar + 'addfight'):
-		tab = message.content.split()
-		name = tab[1]
-		strat = ' '.join(tab[2:])
-		file = open("fights/" + name, "w+")
-		file.write(strat)
-		await message.channel.send("fight was added")
-	elif message.content.startswith(triggerchar + 'fight'):
-		tab = message.content.split()
-		name = tab[1]
-		print("accessing " + name)
-		file = open("fights/" + name, "r")
-		s = file.read()
-		await message.channel.send(s)
+@bot.command(name='newcmd', pass_context='true')
+async def addfight(ctx, name, strat)
+	file = open("fights/" + name, "w+")
+	file.write(strat)
+	await ctx.channel.send("The fight is correctly added")
 
-client.run(token)
+@bot.command(name='fight', pass_context='true')
+async def fight(ctx, name)
+	file = open("fights/"+name, "r")
+	await ctx.channel.send(file.read())
+
+bot.run(token)
